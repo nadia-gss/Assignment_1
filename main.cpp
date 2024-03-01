eda41b934908c2654ed617cd582cccb4a66a23dc
nadia-gss <151856390+nadia-gss@users.noreply.github.com>
2024-03-01 20:53:26 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,26 +6,26 @@ using namespace std;
 using namespace EE513;
 
 int main() {
-    // 创建一个 I2CDevice 对象并连接到指定的 I2C 设备
-    I2CDevice device(1, 0x68);
+	// 创建一个 I2CDevice 对象并连接到指定的 I2C 设备
+	I2CDevice device(1, 0x68);
 
-/**    // 写数据
-    device.writeRegister(0x00, 0xFF);
+	/**    // 写数据
+	 device.writeRegister(0x00, 0xFF);
 
-    // 读取一个字节的数据并打印输出
-    unsigned char data = device.readRegister(0x00);
-    cout << "Data read from register: " << hex << (int)data << endl;
+	 // 读取一个字节的数据并打印输出
+	 unsigned char data = device.readRegister(0x00);
+	 cout << "Data read from register: " << hex << (int)data << endl;
 
-    // 读取多个数据并打印输出
-    unsigned char *registers = device.readRegisters(4, 0x00);
-    cout << "Data read from multiple registers: ";
-    for (int i = 0; i < 4; ++i) {
-        cout << hex << (int)registers[i] << " ";
-    }
-    cout << endl;
+	 // 读取多个数据并打印输出
+	 unsigned char *registers = device.readRegisters(4, 0x00);
+	 cout << "Data read from multiple registers: ";
+	 for (int i = 0; i < 4; ++i) {
+	 cout << hex << (int)registers[i] << " ";
+	 }
+	 cout << endl;
 
-    // 调试输出寄存器的内容
-    device.debugDumpRegisters(16); */
+	 // 调试输出寄存器的内容
+	 device.debugDumpRegisters(16); */
 
 	//打印输出RTC模块的当前时间和日期
 	device.printDateTime();
@@ -33,11 +33,11 @@ int main() {
 	//Read and display the current temperature
 	device.printTemperature();
 	//Get current date and time
-	tm* currentDateTime = device.getSystemDateTime();
+	tm *currentDateTime = device.getSystemDateTime();
 	//Set date and time of RTC
-	tm* time_now = device.setCurrentDateTime();
+	tm *time_now = device.setCurrentDateTime();
 	//Set an alarm
-	device.setAlarm1(0,42,20,1);
+	device.setAlarm1(0, 42, 20, 1);
 
-    return 0;
+	return 0;
 }
